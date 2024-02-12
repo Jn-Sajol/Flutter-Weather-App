e693d0a2be91d75b3b9535759fd834a64da468e1
Jn-Sajol <julkar10121@gmail.com>
2024-02-12 12:52:39 +0600

    complete app banner segment

--- lib/Weather_app.dart
+++ lib/Weather_app.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class WeatherApp extends StatelessWidget{
+class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
 
   @override
@@ -9,20 +8,54 @@ class WeatherApp extends StatelessWidget{
     return Scaffold(
       backgroundColor: Colors.black54,
       appBar: AppBar(
-        title:const Text('Weather App',style: TextStyle(color: Colors.black54),),
+        title: const Text(
+          'Weather App',
+          style: TextStyle(color: Colors.black54),
+        ),
         centerTitle: true,
         // backgroundColor: Colors.black,
         elevation: 4,
         actions: [
           IconButton(
-              onPressed: (){
-                print('refreshed');
-              },
-              icon:const Icon(Icons.refresh),
+            onPressed: () {
+              print('refreshed');
+            },
+            icon: const Icon(Icons.refresh),
           )
         ],
       ),
+      body: Padding(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Card(
+              surfaceTintColor: Colors.red,
+                elevation: 23,
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(12),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Weather App",
+                        style: TextStyle(
+                            fontSize: 34, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.cloud,
+                        size: 65,
+                      ),
+                      Text(
+                        'Rain',
+                        style: TextStyle(fontSize: 44),
+                      ),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
-
 }
